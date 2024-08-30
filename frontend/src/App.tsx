c7b8e8f5dd1e9096b6cf6733bdf2bc00d0257384
Marcel Herr <mrclhrr@gmail.com>
2024-08-30 14:27:27 +0200

    add BookGallery, BookCard and Header placeholder to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,37 @@
 import './App.css'
 import ProductGallery from "./components/productGallery/ProductGallery.tsx";
+import axios from "axios"
+import {useEffect, useState} from "react";
+import {Product} from "./types/types.tsx";
+import Header from "./components/header/Header.tsx";
 
-function App() {
+export default function App() {
 
+    const [data, setData] = useState<Product[]>([])
+
+    const fetchProducts = () => {
+        axios.get("/api/products")
+            .then((response) => {
+                setData(response.data)
+            })
+            .catch((error) => {
+                alert(error)
+            })
+    }
+
+    console.log(data)
+
+    useEffect(() => {
+        fetchProducts()
+    }, []);
 
     return (
         <>
-            <h1>This is a Headline</h1>
-            <ProductGallery/>
+            <Header/>
+            <main>
+                <ProductGallery data={data}/>
+            </main>
         </>
     )
-}
+};
 
-export default App
---
+++ frontend/src/components/productGallery/ProductGallery.tsx
@@ -0,0 +1,21 @@
+import './ProductGallery.css'
+import ProductCard from "../productCard/ProductCard.tsx";
+import {Product} from "../../types/types.tsx";
+
+type ProductGalleryProps = {
+    data: Product[]
+}
+
+export default function ProductGallery({data}: ProductGalleryProps) {
+    if (!data || data.length === 0) {
+        return <p>No Products</p>;
+    }
+
+    return (
+        <ul className="product-list">
+            {data.map((product) => (
+                <ProductCard key={product.id} product={product}/>
+            ))}
+        </ul>
+    );
+}
