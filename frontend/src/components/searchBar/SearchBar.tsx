fab2597c50a6ac076e29995c3cbf39fbc8470939
Marcel Herr <mrclhrr@gmail.com>
2024-09-09 14:14:49 +0200

    fix some typos

--- frontend/src/components/searchBar/SearchBar.tsx
+++ frontend/src/components/searchBar/SearchBar.tsx
@@ -4,12 +4,12 @@ export default function SearchBar({setSearchInput}: any) {
     return (
 
         <div className={"searchBar-container"}>
-            <label htmlFor={"searchbar"} className="sr-only">Search for Title or Author</label>
+            <label htmlFor={"searchbar"} className="sr-only">Search for Product</label>
             <span className="material-symbols-outlined search-lense">search </span>
             <input className={"search-input"}
                    type={"text"}
                    name={"searchbar"}
-                   placeholder={"Search for Title or Author"}
+                   placeholder={"Search for Product"}
                    onChange={(event) => setSearchInput(event.target.value)}
             />
         </div>
