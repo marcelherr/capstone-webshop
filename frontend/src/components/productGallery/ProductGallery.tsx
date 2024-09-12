56003d32d6d94c18516287a8a2e9df4efce55dc9
Marcel Herr <mrclhrr@gmail.com>
2024-09-12 11:09:42 +0200

    add sortOprtions component

--- frontend/src/components/productGallery/ProductGallery.tsx
+++ frontend/src/components/productGallery/ProductGallery.tsx
@@ -3,17 +3,33 @@ import ProductCard from "../productCard/ProductCard.tsx";
 import {Product} from "../../types/types.tsx";
 import SearchBar from "../searchBar/SearchBar.tsx";
 import {Dispatch, SetStateAction} from "react";
+import SortOption from "../sortOptions/SortOption.tsx";
 
 type ProductGalleryProps = {
-    data: Product[]
-    setSearchInput: Dispatch<SetStateAction<string>>
+    data: Product[];
+    setSearchInput: Dispatch<SetStateAction<string>>;
+    toggleSortOrder: () => void;
+    toggleSortPrice: () => void;
+    setIsNameAscending: Dispatch<SetStateAction<boolean | null>>;
+    setIsPriceAscending: Dispatch<SetStateAction<boolean | null>>;
 }
 
-export default function ProductGallery({data, setSearchInput}: ProductGalleryProps) {
+export default function ProductGallery({
+                                           data,
+                                           setSearchInput,
+                                           toggleSortOrder,
+                                           toggleSortPrice,
+                                           setIsNameAscending,
+                                           setIsPriceAscending
+                                       }: ProductGalleryProps) {
     return (
         <>
             <h2>All Products</h2>
             <SearchBar setSearchInput={setSearchInput}/>
+            <SortOption toggleSortOrder={toggleSortOrder}
+                        toggleSortPrice={toggleSortPrice}
+                        setIsNameAscending={setIsNameAscending}
+                        setIsPriceAscending={setIsPriceAscending}/>
             {(!data || data.length === 0) ? (
                 <p>No Products</p>
             ) : (
---
+++ frontend/src/components/sortOptions/SortOption.tsx
@@ -0,0 +1,47 @@
+import './SortOptions.css'
+import {Dispatch, SetStateAction, useState} from "react";
+
+
+type SortOptionProps = {
+    toggleSortOrder: () => void;
+    toggleSortPrice: () => void;
+    setIsNameAscending: Dispatch<SetStateAction<boolean | null>>;
+    setIsPriceAscending: Dispatch<SetStateAction<boolean | null>>;
+}
+
+export default function SortOption({
+                                       toggleSortOrder,
+                                       toggleSortPrice,
+                                       setIsNameAscending,
+                                       setIsPriceAscending
+                                   }: SortOptionProps) {
+
+    const [showSortButton, setShowSortButton] = useState<boolean | null>(null)
+    const [showPriceButton, setShowPriceButton] = useState<boolean | null>(null)
+
+
+    function handleSortButton() {
+        setShowSortButton(!showSortButton)
+        toggleSortOrder();
+    }
+
+    function handlePriceButton() {
+        setShowPriceButton(!showPriceButton)
+        toggleSortPrice();
+    }
+
+    function onReset() {
+        setShowSortButton(null);
+        setShowPriceButton(null);
+        setIsNameAscending(null);
+        setIsPriceAscending(null);
+    }
+
+    return (
+        <div>
+            <button onClick={handleSortButton}> {showSortButton ? "AZ↓" : "ZA↑"}</button>
+            <button onClick={handlePriceButton}> Price {showPriceButton ? "↓" : "↑"}</button>
+            <button onClick={onReset}>Reset</button>
+        </div>
+    )
+}
